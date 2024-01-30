405c8eaaed57e1cce3d71b7fd952b0c712586e63
Maksim P <pojilcov.my@rcitsakha.ru>
2024-01-30 16:01:53 +0900

    Добовление DOM объектов

--- baseDate.js
+++ baseDate.js
@@ -1,48 +1,65 @@
-let formatMessage = (users) => {
-    let usersList = "Список сохраненных пользователей:\n"
-
-    for (let i = 0; i < users.length; i++) {
-        for (let j = 0; j < users[i].length; j++) {
-            usersList += ` ${users[i][j]}`;
-        }
-        usersList += `\n`
-    }
-    return usersList
+function User(name, email) {
+    this.name = name;
+    this.email = email;    
 }
+
+User.prototype.printInfo = function (name, email) {
+    return `Имя: ${this.name},  Эл.почта: ${this.email}`
+}
+
+let formatMessage = (users) => {
+    let list = document.querySelector(".contactList")
+
+
+
+  let usersList = "Список сохраненных пользователей:\n";
+
+  for (let i = 0; i < users.length; i++) {
+    // usersList += ` ${users[i].printInfo()}\n`;
+    const listItem = document.createElement('li')
+    listItem.textContent = `${users[i].name}  ${users[i].email}`
+    list.append(listItem)
+  }
+
+  return usersList;
+};
 
 let createUser = (fields) => {
-    let newUser = []
+  let newUser = {};
 
-    for (let i = 0; i < fields.length; i++) {
-        do {
-            const fieldsValue = prompt(fields[i])
+  for (let i = 0; i < fields.length; i++) {
+    do {
+      const fieldsValue = prompt(fields[i].message);
 
-            if (fieldsValue) {
-                newUser.push(fieldsValue)
-                break
-            }
-        } while (true);
-    }
-    return newUser
-}
-
+      if (fieldsValue) {
+        newUser[fields[i].key] = fieldsValue;
+        break;
+      }
+    } while (true);
+  }
+  
+  return new User(newUser.name, newUser.email);
+  
+};
 
 let init = () => {
-    const users = []
-    let fields = ["Введите имя", "Введите почту"]
-    let isCreate
+  const users = [];
+  let fields = [
+    { key: "name", message: "Введите имя" },
+    { key: "email", message: "Введите почту" },
+  ];
+  let isCreate;
 
-    do {
-        isCreate = confirm("Хотите создать нового пользователя?")
+  do {
+    isCreate = confirm("Хотите создать нового пользователя?");
 
-        if (!isCreate) break
+    if (!isCreate) break;
 
-        const newUser = createUser(fields)
-        users.push(newUser)
-    } while (true);
+    const newUser = createUser(fields);
+    users.push(newUser);
+  } while (true);
 
-    
-    alert(formatMessage(users))
-}
+  formatMessage(users);
+};
 
-init()
+init();
