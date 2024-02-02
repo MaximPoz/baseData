6594f03475a56eb95475235d67d8bea6ef751d80
Maksim P <pojilcov.my@rcitsakha.ru>
2024-02-02 18:08:05 +0900

    class

--- baseDate.js
+++ baseDate.js
@@ -2,29 +2,27 @@ const h1 = document.querySelector("h1");
 
 // h1.addEventListener("click", () => (h1.textContent = "Удалено")); //! балуюсь
 
-function User(name, email) {
-  this.name = name;
-  this.email = email;
+class User {
+  constructor({ name, email }) {
+    this.name = name;
+    this.email = email;
+  }
+  getInfo() {
+    return `Имя: ${this.name},  Эл.почта: ${this.email}`;
+  }
 }
-
-User.prototype.printInfo = function () {
-  return `Имя: ${this.name},  Эл.почта: ${this.email}`;
-};
-
 
 let renderUserList = (users) => {
   let list = document.querySelector(".users-list");
 
-    list.innerHTML = "";
+  list.innerHTML = "";
 
-  for (let i = 0; i < users.length; i++) {
+  users.forEach((user) => {
     const listItem = document.createElement("li");
-
-    listItem.textContent = `${users[i].name}  ${users[i].email}`;
+    listItem.textContent = user.getInfo();
     list.append(listItem);
-  }
+  });
 };
-
 
 let init = () => {
   const users = [];
@@ -38,7 +36,7 @@ let init = () => {
     const email = emailEL.value;
 
     if (name && email) {
-      const user = new User(name, email);
+      const user = new User({ name, email });
       users.push(user);
       renderUserList(users);
       nameEL.value = "";
@@ -47,38 +45,30 @@ let init = () => {
   });
 };
 
-let createBtn1 = document.querySelector(".create-btn");
-const move = createBtn1.animate(
-  [
-    { transform: "translateX(0px)" },
-    { transform: "translateX(100px)" },
-    { transform: "translateX(0px)" },
-  ],
-  {
-    duration: 3000,
-    iterations: Infinity,
-  }
-)
-
-
-createBtn1.addEventListener("mouseenter", () => {
-  move.pause();
-});
-createBtn1.addEventListener("mouseleave", () => {
-  move.play();
-});
+// let createBtn1 = document.querySelector(".create-btn"); //! кнопка двигается по горизонтали
+// const move = createBtn1.animate(
+//   [
+//     { transform: "translateX(0px)" },
+//     { transform: "translateX(100px)" },
+//     { transform: "translateX(0px)" },
+//   ],
+//   {
+//     duration: 3000,
+//     iterations: Infinity,
+//   }
+// );
+// createBtn1.addEventListener("mouseenter", () => {
+//   move.pause();
+// });
+// createBtn1.addEventListener("mouseleave", () => {
+//   move.play();
+// });
 
 let logo = document.querySelector(".logo");
 const moveLogo = logo.animate(
-  [
-    { transform: "rotate(0deg)" },
-    { transform: "rotate(360deg)" },
-  ],
-  { duration: 3000, iterations: Infinity }
-)
-
-
-
+  [{ transform: "rotate(0deg)" }, { transform: "rotate(360deg)" }],
+  { duration: 5000, iterations: Infinity }
+);
 
 logo.addEventListener("mouseenter", () => {
   moveLogo.pause();
